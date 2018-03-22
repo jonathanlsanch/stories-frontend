4c433ce6c5cee362a7e7e4b44fd1c7b93bbab44e
Jonathan Sanchez <jonathansanchez@Jonathans-MacBook-Pro.local>
2018-03-22 17:27:42 -0400

    Letters by category working

--- src/app/components/howitworks/howitworks.component.ts
+++ src/app/components/howitworks/howitworks.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router";
+import { AuthService } from "../../services/auth.service";
+import { StoryService } from "../../services/story.service";
+// import { environment } from "../../../environments/environment";
 
 @Component({
   selector: 'app-howitworks',
@@ -6,10 +10,55 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./howitworks.component.css']
 })
 export class HowitworksComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+    logoutError: string;
+    storyListError: string;
+    stories: any;
+    currentUser: string;
+  
+    constructor(
+      private myAuthService: AuthService,
+      private myRouter: Router,
+      private myStoryService: StoryService
+    ) {}
+  
+    ngOnInit() {
+      this.myAuthService
+        .checklogin()
+        // If success, we are logged in.
+        .then(resultFromApi => {
+          this.currentUser = resultFromApi;
+          console.log("user is: ", resultFromApi);
+          this.getTheStories()
+        })
+  
+        // Even if you don't do anything on error, catch to avoid a console error.
+        .catch(err => {
+          console.log(err);
+          this.myRouter.navigate(["/"]);
+        });
+      // this.getTheStories();
+    }
+  
+    getTheStories() {
+      this.myStoryService.getAllStories()
+      .subscribe(allTheStories => {
+        // console.log("allTheStories: ", allTheStories)
+          this.stories = allTheStories;
+          console.log("stories", this.stories)
+        },
+        () => {
+          this.storyListError = "Sorry, no stories.";
+        }
+      );
+    } // close getTheStories()
+    logMeOutPls() {
+      this.myAuthService
+        .logout()
+        .then(() => {
+          this.myRouter.navigate(["/"]);
+        })
+        .catch(() => {
+          this.logoutError = "Log out went bad.";
+        });
+    } // close logMeOutPls()
 }
